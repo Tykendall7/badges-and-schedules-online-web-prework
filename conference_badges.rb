a8bebb1dcdd5729319e05b6df80d33b08a73d565
Tymeckia Kendall <tykend@gmail.com>
2018-12-01 22:43:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
 # Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def   badge_maker (name)
+   attendees.each do { |name| put "Hello" #{name}"}
+  end 
+end 
+
